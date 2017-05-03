be2cd7e8954bf02f314758305512f1ea9a564ab4
Yannick Heinrich <yannick.heinrich@gmail.com>
2017-05-03 16:09:23 +0200

    Initial Commit

---
+++ LazyBug/LazyBug.h
@@ -0,0 +1,19 @@
+//
+//  LazyBug.h
+//  LazyBug
+//
+//  Created by Yannick Heinrich on 03.05.17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LazyBug.
+FOUNDATION_EXPORT double LazyBugVersionNumber;
+
+//! Project version string for LazyBug.
+FOUNDATION_EXPORT const unsigned char LazyBugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LazyBug/PublicHeader.h>
+
+
